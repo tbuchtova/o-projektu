b728ece69f4a13c93f906546f8cb5b55234728e4
Tereza Plecitá <plecita.terez@gmail.com>
2022-05-22 18:09:06 +0200

    Úprava headeru - změna barevnosti, loga a zarovnání

--- src/Header/index.jsx
+++ src/Header/index.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
-import bookIcon from '../img/book-icon.svg';
+import bookIcon from '../img/books.png';
 import './style.css';
 
 const Header = () => {
   return (
     <>
-      <header className="header">
-        <h1>Booklog</h1>
-        <img className="header-icon" src={bookIcon} alt="book-icon" />
+      <header>
+        <div className="header">
+          <h1>Booklog</h1>
+          <img className="header-icon" src={bookIcon} alt="book-icon" />
+        </div>
       </header>
     </>
   );
